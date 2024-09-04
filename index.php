b41388c37e6f000e006b88d11b1242570f7151e5
Dloger <n.kovyazin@wizart.com>
2024-09-04 09:24:15 +0300

    create main page

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-echo "test";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,6 +9,10 @@ echo "test";
 	<title>Document</title>
 </head>
 <body>
-	
+	<ul>
+	 	<li><a href="/1">Задание 1</a></li>
+	 	<li><a href="/2">Задание 2</a></li>
+	 	<li><a href="/3">Задание 3</a></li>
+	 </ul> 
 </body>
 </html>

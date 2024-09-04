962d9d8653486fdf6f245e5e3c0883fc112a9adb
Dloger <n.kovyazin@wizart.com>
2024-09-04 11:46:04 +0300

    explede fileto array and output count number

--- 1/index.php
+++ 1/index.php
@@ -16,14 +16,36 @@
 	
 	<?php
 		if (array_key_exists('file', $_FILES)) {
-			echo "load file <br>";
-			$fileName = $_SERVER['DOCUMENT_ROOT'] . '/1/files/' . date('Y_m_d_h_i_s_u') . '.txt';
+			$fileName = $_SERVER['DOCUMENT_ROOT'] . '/1/files/' . date('Y_m_d_h_i_s') . '.txt';
 			$resWrite = file_put_contents($fileName, file_get_contents($_FILES['file']['tmp_name']));
 			if ($resWrite > 0) {
-				echo "file save <br>";
-				
+				?>
+					<div class="container">
+						<div class="success circle"></div>
+					</div>
+				<?php
+
+				$fileData = file_get_contents($fileName);
+				if ($fileData) {
+					$data = explode(PHP_EOL, $fileData);
+					$pattern = '/[^0-9]/';
+					?>
+						<div class="result">
+					<?php
+					foreach ($data as $str) {
+						$strNumberLen = strlen(preg_replace($pattern, "", $str));
+						echo "{$str} = {$strNumberLen}<br>";
+					}
+					?> 
+						</div>
+					<?php
+				}
 			} else {
-				echo "error load file <br>";
+				?>
+					<div class="container">
+						<div class="error circle"></div>
+					</div>
+				<?php
 			}
 		} 
 	?>
